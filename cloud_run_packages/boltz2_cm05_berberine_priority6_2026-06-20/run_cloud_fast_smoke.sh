696efef9f203a251f840886832a196a612ebd2fb
lhs1308781604-droid <lhs1308781604@gmail.com>
2026-06-20 21:55:51 +0800

    Enable MSA server for CM05 Boltz2 smoke

--- cloud_run_packages/boltz2_cm05_berberine_priority6_2026-06-20/run_cloud_fast_smoke.sh
+++ cloud_run_packages/boltz2_cm05_berberine_priority6_2026-06-20/run_cloud_fast_smoke.sh
@@ -46,7 +46,7 @@ mkdir -p cloud_results/logs cloud_results/status cloud_results/tables cloud_resu
   export BOLTZ_RUN_MODE=smoke
   export BOLTZ_ACCELERATOR=cpu
   export BOLTZ_DEVICES=1
-  export BOLTZ_USE_MSA_SERVER=0
+  export BOLTZ_USE_MSA_SERVER=1
   export BOLTZ_USE_POTENTIALS=0
   export BOLTZ_FORCE_NO_TEMPLATE=1
   export BOLTZ_ALLOW_FALLBACK=1
@@ -57,8 +57,8 @@ mkdir -p cloud_results/logs cloud_results/status cloud_results/tables cloud_resu
   export BOLTZ_DIFFUSION_SAMPLES_AFFINITY=1
   export BOLTZ_SEED=20260620
 
-  echo "fast_smoke_params: cpu no_template no_msa no_potentials recycling=1 sampling=5 affinity_sampling=5"
-  boltz_timeout="${BOLTZ_FAST_SMOKE_TIMEOUT_SECONDS:-600}"
+  echo "fast_smoke_params: cpu no_template msa_server no_potentials recycling=1 sampling=5 affinity_sampling=5"
+  boltz_timeout="${BOLTZ_FAST_SMOKE_TIMEOUT_SECONDS:-1800}"
   echo "boltz_fast_smoke_timeout_seconds: $boltz_timeout"
   timeout "$boltz_timeout" python scripts/run_boltz2_cloud.py
   boltz_status=$?
